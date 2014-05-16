f499a30fc9d523cc08f56ccd09e005a72797f9e3
gzfrancisco <gz.francisco@gmail.com>
2014-05-15 20:48:48 -0500

    Fix menu in phone

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function header_on_touchend (event) {
+  var sidebar = document.getElementsByTagName('aside');
+  var display = sidebar[0].style.display;
+  sidebar[0].style.display = (display == 'block')?'none':'block';
+}
+function init (event) {
+  var header = document.getElementsByTagName('header');
+  header[1].addEventListener('touchend', header_on_touchend);
+}
+window.onload = init;
