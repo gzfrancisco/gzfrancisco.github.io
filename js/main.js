6bfb243fcfdaeaf240eeafac2cf9e1b7d400ed97
gzfrancisco <gz.francisco@gmail.com>
2014-05-15 23:22:55 -0500

    Fix images and fix touch event

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 function header_on_touchend (event) {
+  event.preventDefault();
   var sidebar = document.getElementsByTagName('aside');
   var display = sidebar[0].style.display;
   sidebar[0].style.display = (display == 'block')?'none':'block';
